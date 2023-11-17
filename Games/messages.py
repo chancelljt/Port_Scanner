7480d59551e7961819f1d9a9388b1906279fcf35
Jarrett Chancellor <111771801+chancelljt@users.noreply.github.com>
2023-11-17 15:09:24 -0600

    Delete Games directory

--- Games/messages.py
+++
@@ -1,8 +0,0 @@
-Win_message = "Congragulations, You win!"
-Win_Guess = "Congragulations, You guessed right!"
-Lose_message = "Sorry, you lose. Please try again."
-Lose_Guess = "Sorry, you guessed wrong. Please try again."
-
-Play_again_message = "Do you want to play again? Enter yes/no: "
-yes_again_message = "Okay, Starting new game! Good Luck!"
-not_again_message = "Thank you for playing!"
